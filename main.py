110bdd163f3ae3cbc046dd1e4794a59832429aae
Ja Tar <tarnogorskijan@gmail.com>
2024-01-24 21:21:57 +0100

    działa?

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 import xml.etree.ElementTree as ET
 
 # Lista linków do plików SVG
@@ -6,13 +7,33 @@ svg_links = [
     "https://upload.wikimedia.org/wikipedia/commons/2/2f/Podstawa_semafor-C2.svg"
 ]
 
-# Tworzenie głównego elementu SVG
-root = ET.Element("svg", xmlns="http://www.w3.org/2000/svg")
+# Pusta lista do przechowywania treści plików SVG
+svg_contents = []
 
-# Iteracja przez listę linków i dodanie każdego pliku SVG pod poprzednim
-for i, link in enumerate(svg_links):
-    # Tworzenie elementu <image> dla każdego pliku SVG
-    image_element = ET.SubElement(root, "image", {"href": link, "y": str(i * 28)})  # Ustawienie y dla każdego obrazu
+# Pobieranie treści plików SVG
+for link in svg_links:
+    response = requests.get(link)
+    if response.status_code == 200:
+        svg_contents.append(response.content)
+    else:
+        print(f"Nie udało się pobrać pliku SVG z linku: {link}")
+
+# Tworzenie głównego elementu SVG
+root = ET.Element("svg", xmlns="http://www.w3.org/2000/svg", attrib={"version": "1.1", "encoding": "UTF-8"})
+
+# Iteracja przez treści pobranych plików SVG i dodanie ich do głównego SVG
+for i, content in enumerate(svg_contents):
+    try:
+        # Parsowanie treści pliku SVG
+        svg_tree = ET.fromstring(content)
+
+        # Tworzenie grupy (g) dla każdego pliku SVG
+        svg_group = ET.SubElement(root, "g", {"transform": f"translate(0, {i * 28})"})
+
+        # Dodanie elementów z parsowanego pliku SVG do grupy
+        svg_group.extend(svg_tree)
+    except ET.ParseError:
+        print(f"Nie udało się sparsować pliku SVG z linku: {svg_links[i]}")
 
 # Ustawienie szerokości i wysokości obszaru SVG
 root.set("width", "50")
@@ -22,4 +43,4 @@ root.set("height", str(len(svg_links) * 28))  # Ustawienie wysokości na całkow
 tree = ET.ElementTree(root)
 
 # Zapisywanie do pliku SVG
-tree.write("combined_svg.svg")
+tree.write("combined_svg.svg", encoding="utf-8", xml_declaration=True)
